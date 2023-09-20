f8c0d70d7fd32d41aa9a1a389546e8d15e1fdd42
Pedro Teixeira <123015308+PedroTeixeira-dev@users.noreply.github.com>
2023-09-20 10:15:12 -0300

    Avancando na aula de grunt

--- gruntfile.js
+++ gruntfile.js
@@ -19,14 +19,75 @@ module.exports = function(grunt) {
         watch: {
             less: {
                 files: ['src/styles/**/*.less'], // quando se usa '/**' estamos referenciando qualquer pasta e quando usamos /* estamos referenciando qualquer arquivo
-                tasks: ['less:development']  
+                tasks: ['less:development']                
+            },
+            html: {
+                files: ['src/index.html'],
+                tasks: ['replace:dev']
             }
-        } 
+        },
+        replace:{
+            dev: {
+                options:{
+                    patterns:[
+                        {
+                            match:'ENDERECO_DO_CSS',
+                            replacement:'./styles/main.css'  
+                        },
+                        {
+                            match:'ENDERECO_DO_JS',
+                            replacement:'../src/scripts/main.js'  
+                        }
+                    ]
+                },
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['src/index.html'],
+                        dest: 'dev/'
+                    }
+                ]
+            },
+            dist: {
+                options:{
+                    patterns:[
+                        {
+                            match:'ENDERECO_DO_CSS',
+                            replacement:'./styles/main.min.css'  
+                        }
+                    ]
+                },
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['prebuild/index.html'],
+                        dest: 'dist/'
+                    }
+                ]
+            }
+        },
+        htmlmin: {
+            dist: {
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: {
+                    'prebuild/index.html': 'src/index.html'
+                }
+            }
+        },
+        clean: ['prebuild']
     })
 
     grunt.loadNpmTasks('grunt-contrib-less') //importando o plugin do less
     grunt.loadNpmTasks('grunt-contrib-watch') //importando o plugin do watch
+    grunt.loadNpmTasks('grunt-replace') // importando o plugin de substituir
+    grunt.loadNpmTasks('grunt-contrib-htmlmin') // importando o arquivo de minificacao do html
+    grunt.loadNpmTasks('grunt-contrib-clean') //usado para limpar pastas, no caso, as temporarias
 
     grunt.registerTask('default', ['watch'])
-    grunt.registerTask('build', ['less:production'])
+    grunt.registerTask('build', ['less:production','htmlmin:dist','replace:dist','clean'])
 }
